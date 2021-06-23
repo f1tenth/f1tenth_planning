5e374055a98e6d4710b585dfb304d124f3954d7f
billyzheng-mlab <billyzheng.bz@gmail.com>
2021-06-23 17:57:59 -0400

    update lattice planner structure

--- f1tenth_lattice_planner/f1tenth_lattice_planner/lattice_planner.py
+++ f1tenth_lattice_planner/f1tenth_lattice_planner/lattice_planner.py
@@ -30,6 +30,7 @@ from f1tenth_planner_base.base import Planner
 from f1tenth_pure_pursuit.pure_pursuit import PurePursuitPlanner
 from f1tenth_planner_base.utils import nearest_point
 from f1tenth_planner_base.utils import intersect_point
+from f1tenth_planner_base.utils import quat_2_rpy
 
 ROS2_PRESENT = True
 try:
@@ -69,12 +70,115 @@ class LatticePlanner(Planner):
     lut_k = lut['kappa']
     lut = lut['lut']
 
-    tracker = PurePursuitPlanner()
-
     def __init__(self, conf):
         super().__init__(conf)
+        self.tracker = PurePursuitPlanner(conf)
+        self.cost_funcs = []
 
-    def _get_current_waypoint(self):
+    # private
+    def _get_current_grid(self, x, y, theta, kappa):
+        """
+        Gets the current look ahead grid for spline generation
+        
+        Args:
+            x, y, theta (float): current pose of the vehicle in world frame
+            kappa (float): current steering angle of the vehicle
+
+        Returns:
+            grid (numpy.ndarray (N x 4)): indices in the LUT for the grid end points
+                 each columns is index for: x, y, theta, kappa 
+        """
+        pass
+
+    def _evaluate_costs(self, trajectories):
+        """
+        Evaluates the costs of each splines in the current grid
+        
+        Args:
+            trajectories (numpy.ndarray (N*N_points X 2)): trajectories (x, y) stacked row wise
+
+        Returns:
+            selected_trajectory (numpy.ndarray (N_points X 2)): selected trajectory with the lowest cost
+        """
+        pass
+
+    def _get_actuation(self):
+        """
+        Calculates the actuation to follow the current selected trajectory
+
+        Args:
+            None
+
+        Returns:
+            speed, steer (float): current desired speed and steering angle
+        """
+        pass
+
+    def _generate_splines(self, grid):
+        """
+        Takes the current grid and generate the splines
+
+        Args:
+            grid (numpy.ndarray (N X 4)): indices in the LUT for the grid end points
+
+        Returns:
+            states (numpy.ndarray (N*N_points X 2)): trajectories (x, y) stacked row wise
+        """
+        pass
+
+    # public
+    def add_cost_function(self, func):
+        """
+        Adds a term in the trajectory cost calculation by adding a function to evaluate to the list
+        # TODO: need a fix signature for functions
+
+        Args:
+            func: function that calculates a spline's cost
+        """
+        pass
+
+    def get_current_plan(self):
+        """
+        Returns information on the current lookup grid, corresponding costs, and the selected trajectory
+        
+        Args:
+            None
+
+        Returns:
+            grid (numpy.ndarray (num_grid_points X 2)): (x, y) points of the grid
+            costs (numpy.ndarray (num_grid_points, )): cost of splines corresponding to the index in grid
+            selected (int): selected index of the grid
+        """
+        pass
+
+    def plan(self, x, y, theta, **kwargs):
+        """
+        Overloads the inherited plan function.
+
+        Args:
+            x, y, theta (float): current pose of the vehicle in world frame
+            **kwargs: keyword arguments, the following are used for this specific planner:
+
+                opp_x (float): opponent pose x
+                opp_y (float): opponent pose y
+                opp_theta (float): opponent pose theta
+
+                glad (float): look ahead distance for center of grid
+                gwidth (float): width of lookup grid
+                glength (float): length of lookup grid
+                
+                update_plan (bool): whether the current plan should be updated or not
+                                    if True, a new trajectory is selected
+                                    if False, the current trajectory will be followed
+                                    will be overwritten if the current plan is at the EOL
+
+                TODO: need something for velocity
+
+        Returns:
+            speed, steer (float): current desired speed and steering angle
+        """
+        pass
+
 
 
 if ROS2_PRESENT:
@@ -95,6 +199,7 @@ if ROS2_PRESENT:
             self.conf['wpt_vind'] = self.get_parameter('wpt_vind').value
             self.conf['use_csv'] = self.get_parameter('use_csv').value
             self.conf['wpt_path'] = self.get_parameter('wpt_path').value
+            # TODO: change these (yaml) for lattice planner
             self.conf['lookahead_distance'] = self.get_parameter('lookahead_distance').value
             self.conf['wheelbase'] = self.get_parameter('wheelbase').value
 
@@ -131,10 +236,10 @@ if ROS2_PRESENT:
 # node definition
 def main(args=None):
     rclpy.init(args=args)
-    pure_pursuit_node = ROS2PurePursuit()
-    rclpy.spin(pure_pursuit_node)
+    lattice_node = ROS2Lattice()
+    rclpy.spin(lattice_node)
 
-    pure_pursuit_node.destroy_node()
+    lattice_node.destroy_node()
     rclpy.shutdown()
 
 # fall back main for no ROS2
