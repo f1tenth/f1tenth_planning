d45577f7dc07c3f9f6b316216a35cda1662ff769
billyzheng <billyzheng.bz@gmail.com>
2022-05-01 21:54:45 -0400

    remove ros2 packages

--- f1tenth_lattice_planner/f1tenth_lattice_planner/download_files.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-
-./gdown.pl 'https://drive.google.com/open?id=1hW3X9IGq7fu_1aw6joTI_WU6_gai7Pwy' lut_inuse.npz
--- python_uninstall.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-pip3 uninstall f1tenth_planner_base f1tenth_pure_pursuit f1tenth_stanley
