f2f262b6d33e342fd27b16bc14fdcb7163d7783e
billyzheng-mlab <billyzheng.bz@gmail.com>
2022-06-06 18:37:53 -0400

    update

--- f1tenth_planning/utils/utils.py
+++ f1tenth_planning/utils/utils.py
@@ -294,41 +294,8 @@ def sample_traj(clothoid, npts):
 
     return traj
 
-# def sample_lookahead_square(pose_x,
-#                             pose_y,
-#                             pose_theta,
-#                             velocity,
-#                             waypoints,
-#                             lookahead_distances=[0.4, 0.6, 0.8, 1.0],
-#                             widths=np.linspace(-1.0, 1.0, num=7)):
-#     """
-#     Example function to sample goal points. In this example it samples a rectangular grid around a look-ahead point.
-
-#     Args:
-#         pose_x ():
-#         pose_y ():
-#         pose_theta ():
-#         velocity ():
-#         waypoints ():
-#         lookahead_distances ():
-#         widths ():
-    
-#     Returns:
-#         grid (): Returned grid of goal points
-#     """
-#     # get lookahead points to create grid along waypoints
-#     position = np.array([pose_x, pose_y])
-#     nearest_p, nearest_dist, t, i = nearest_point(position, waypoints[:, 0:2])
-#     lh_centers = []
-#     for i, d in enumerate(lookahead_distances):
-#         lh_pt, i2, t2 = intersect_point(position, d, waypoints[:, 0:2], i + t, wrap=True)
-#         lh_centers[i] = waypoints[i2, [0, 1, 3]]
-#     lh_centers = np.array(lh_centers)
-#     grid = np.repeat(lh_centers, len(widths), axis=0)
-#     widths_rep = np.repeat(widths[:, None], lh_centers.shape[0], axis=0)
-#     # deviate points from center
-#     grid[:, 1] += widths_rep[:, 0]
-#     # rotate grid
-#     rot = get_rotation_matrix(pose_theta)
-#     rotated_grid = np.dot(rot, grid)
-#     return rotated_grid
+def map_collision(point, map):
+    """
+    Returns whether a point is in collision with the map
+    """
+    pass
