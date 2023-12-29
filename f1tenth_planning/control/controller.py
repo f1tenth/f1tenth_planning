61f552ba7cb9dc147109ad4d5a6753040832eb34
luigiberducci <luigi.berd@gmail.com>
2023-12-29 16:13:55 +0100

    add color property

--- f1tenth_planning/control/controller.py
+++ f1tenth_planning/control/controller.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from abc import abstractmethod, ABC
 
 import numpy as np
@@ -13,10 +14,24 @@ class Controller(ABC):
     def plan(self, state: dict) -> np.ndarray:
         raise NotImplementedError("control method not implemented")
 
+    @property
+    def color(self) -> tuple[int, int, int]:
+        """
+        Color for rendering waypoints of this controller.
+        """
+        return 128, 0, 0
+
+    @color.setter
+    def color(self, value: tuple[int, int, int]) -> None:
+        """
+        Color for rendering waypoints of this controller.
+        """
+        self.color = value
+
     def render_waypoints(self, e):
         """
         Callback to render waypoints.
         """
         if self.waypoints is not None:
             points = self.waypoints[:, :2]
-            e.render_closed_lines(points, color=(128, 0, 0), size=1)
+            e.render_closed_lines(points, color=self.color, size=1)
