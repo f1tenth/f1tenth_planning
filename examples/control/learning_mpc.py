a928a087f7bbdc666726603d72a27a8d4c510ba7
Ahmad Amine <AhmadAmine998@gmail.com>
2024-01-17 14:32:03 -0500

    Implement example lmpc init with kmpc

---
+++ examples/control/learning_mpc.py
@@ -0,0 +1,155 @@
+# MIT License
+
+# Copyright (c) Ahmad Amine
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""
+LMPC example
+
+Author: Ahmad Amine
+Last Modified: 8/1/22
+"""
+
+import numpy as np
+import gymnasium as gym
+from f110_gym.envs import F110Env
+import time
+import os
+import sys
+import matplotlib.pyplot as plt
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../")
+
+from f1tenth_planning.control.kinematic_mpc.kinematic_mpc import KMPCPlanner
+from f1tenth_planning.control.learning_mpc.learning_mpc import LMPCPlanner
+
+def main():
+    """
+    STMPC example. This example uses fixed waypoints throughout the 2 laps.
+    For an example using dynamic waypoints, see the lane switcher example.
+    """
+
+    features = [
+        "pose_x",
+        "pose_y",
+        "delta",
+        "linear_vel_x",
+        "pose_theta",
+        "ang_vel_z",
+        "beta",
+        "lap_time",
+        "lap_count",
+    ]
+    # create environment
+    env : F110Env = gym.make('f110_gym:f110-v0',
+                            config={
+                                "map": "Rounded_Rectangle",
+                                "num_agents": 1,
+                                "control_input": "accl",
+                                "observation_config": {"type": "features", 
+                                                       "features": features},
+                            },
+                            render_mode='human')
+
+    # create planner
+    planner = KMPCPlanner(track=env.track, debug=False)
+    planner.config.dlk = env.track.raceline.ss[1] - env.track.raceline.ss[0] # waypoint spacing
+    env.unwrapped.add_render_callback(planner.render_waypoints)
+    env.unwrapped.add_render_callback(planner.render_local_plan)
+    env.unwrapped.add_render_callback(planner.render_mpc_sol)
+
+    # reset environment
+    poses = np.array(
+        [
+            [
+                env.track.raceline.xs[0],
+                env.track.raceline.ys[0],
+                env.track.raceline.yaws[0],
+            ]
+        ]
+    )
+    obs, info = env.reset(options={"poses": poses})
+    done = False
+    env.render()
+
+    total_time = 0.0
+    desired_laps = 5
+
+    xSS = []
+    uSS = []
+    vSS = []
+
+    curr_trajectory = np.zeros((0, 7))
+    curr_controls = np.zeros((0, planner.config.NU))
+
+    lap_count = 0
+    start = time.time()
+    first_done = False
+    while not done:
+        curr_state = np.array([
+                                [obs["agent_0"]["pose_x"],
+                                obs["agent_0"]["pose_y"],
+                                obs["agent_0"]["delta"],
+                                obs["agent_0"]["linear_vel_x"],
+                                obs["agent_0"]["pose_theta"],
+                                obs["agent_0"]["ang_vel_z"],
+                                obs["agent_0"]["beta"]]
+                              ])        
+        steerv, accl = planner.plan(obs["agent_0"])
+        obs, timestep, terminated, truncated, infos = env.step(np.array([[steerv, accl]]))
+        done = terminated or truncated
+        env.render()
+
+        curr_controls = np.vstack((curr_controls, np.array([[steerv, accl]])))
+        curr_trajectory = np.vstack((curr_trajectory, curr_state))
+
+        print("speed: {}, steer vel: {}, accl: {}".format(obs["agent_0"]['linear_vel_x'], steerv, accl))
+
+        if lap_count != obs["agent_0"]["lap_count"]:
+            lap_count = obs["agent_0"]["lap_count"]
+            lap_time = obs["agent_0"]["lap_time"] - total_time
+            print("laps_completed: {} | lap_time: {}".format(obs["agent_0"]["lap_count"], lap_time))
+            total_time = obs["agent_0"]["lap_time"]
+            print("Done: {}, Terminated: {}, Truncated: {}".format(done, terminated, truncated))
+
+            # Increasing from 0 to length of trajectory 
+            curr_values = np.array([np.arange(0, curr_trajectory.shape[0], 1)])
+            xSS.append(curr_trajectory)
+            uSS.append(curr_controls)
+            vSS.append(curr_values)
+
+            curr_trajectory = np.zeros((0, 7))
+            curr_controls = np.zeros((0, planner.config.NU))
+
+            time.sleep(3)
+
+        if done and (obs["agent_0"]["lap_count"] != desired_laps):
+            done = False
+            terminated = False
+            
+    # Scatter xSS x,y colored with vSS
+    for i in range(len(xSS)):
+        plt.scatter(xSS[i][:,0], xSS[i][:,1], c=vSS[i])
+    plt.show()
+
+    print("Sim elapsed time:", total_time, "Real elapsed time:", time.time() - start)
+
+
+if __name__ == '__main__':
+    main()
