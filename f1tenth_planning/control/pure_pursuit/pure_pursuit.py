2f9ad2ee75d5611a0ce3a5864318c9e9fd4c11bf
Ahmad Amine <AhmadAmine998@gmail.com>
2024-04-26 03:29:49 -0400

    Implement pure_pursuit controller as inherting from ABC

--- f1tenth_planning/control/pure_pursuit/pure_pursuit.py
+++ f1tenth_planning/control/pure_pursuit/pure_pursuit.py
@@ -1,73 +1,106 @@
-# MIT License
-
-# Copyright (c) Hongrui Zheng, Johannes Betz
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-"""
-Pure Pursuit waypoint tracker
-
-Author: Hongrui Zheng
-Last Modified: 5/4/22
-"""
-
 from f1tenth_planning.utils.utils import nearest_point
 from f1tenth_planning.utils.utils import intersect_point
 from f1tenth_planning.utils.utils import get_actuation
 
+from f1tenth_planning.control.controller import Controller
+from f1tenth_gym.envs.track import Track
+
+import yaml
+import pathlib
 import numpy as np
 import warnings
 
-
-class PurePursuitPlanner:
+class PurePursuitController(Controller):
     """
     Pure pursuit tracking controller
     Reference: Coulter 1992, https://www.ri.cmu.edu/pub_files/pub3/coulter_r_craig_1992_1/coulter_r_craig_1992_1.pdf
 
     All vehicle pose used by the planner should be in the map frame.
 
-    Args:
-        waypoints (numpy.ndarray [N x 4], optional): static waypoints to track
+    Parameters
+    ----------
+        track : Track
+            track object with raceline/centerline
+        config : dict | str, optional
+            dictionary or path to yaml with controller specific parameters, by default None 
+            expects the following keys : 
+                 "wheelbase": float, wheelbase of the vehicle
+                 "lookahead_distance": float, lookahead distance for the controller
+                 "max_reacquire": float, maximum radius for reacquiring current waypoints
 
-    Attributes:
+    Attributes
+    ----------
         max_reacquire (float): maximum radius (meters) for reacquiring current waypoints
         waypoints (numpy.ndarray [N x 4]): static list of waypoints, columns are [x, y, velocity, heading]
     """
 
-    def __init__(self, wheelbase=0.33, waypoints=None):
-        self.max_reacquire = 20.0
-        self.wheelbase = wheelbase
-        self.waypoints = waypoints
-        self.drawn_waypoints = []
+    def __init__(self, track: Track, config: dict | str | pathlib.Path = None) -> None:
+        """Controller init
+
+        Parameters
+        ----------
+        track : Track
+            track object with raceline/centerline
+        config : dict | str, optional
+            dictionary or path to yaml with controller specific parameters, by default None
+            expects the following key:
+            - wheelbase: float, wheelbase of the vehicle
+            - lookahead_distance: float, lookahead distance for the controller
+            - max_reacquire: float, maximum radius for reacquiring current waypoints
+
+        Raises
+        ------
+        ValueError
+            if track is None or does not have waypoints (raceline or centerline)
+        ValueError
+            if config file does not exist
+        """
+        if track is None or (track.raceline is None and track.centerline is None):
+            raise ValueError("Track object with waypoints is required for the controller")
+        
+        # Extract waypoints from track
+        reference = track.raceline if track.raceline is not None else track.centerline
+        self.waypoints = np.stack(
+                            [reference.xs, reference.ys, reference.vxs, reference.yaws], axis=1
+                        )
+        
+        if config is not None:
+            if isinstance(config, (str, pathlib.Path)):
+                if isinstance(config, str):
+                    config = pathlib.Path(config)
+                if not config.exists():
+                    raise ValueError(f"Config file {config} does not exist")
+                config = self.load_config(config)
+        else:
+            config = {}
+
+        self.wheelbase = config.get("wheelbase", 0.33)
+        self.lookahead_distance = config.get("lookahead_distance", 0.8)
+        self.max_reacquire = config.get("max_reacquire", 5.0)
+
         self.lookahead_point = None
         self.current_index = None
 
-    def render_waypoints(self, e):
-        """
-        Callback to render waypoints.
-        """
-        points = self.waypoints[:, :2]
-        e.render_closed_lines(points, color=(128, 0, 0), size=1)
+    def update(self, config: dict) -> None:
+        """Updates setting of controller
 
+        Parameters
+        ----------
+        config : dict
+            configurations to update
+        """
+        self.wheelbase = config.get("wheelbase", self.wheelbase)
+        self.lookahead_distance = config.get("lookahead_distance", self.lookahead_distance)
+        self.max_reacquire = config.get("max_reacquire", self.max_reacquire)
+        
     def render_lookahead_point(self, e):
         """
         Callback to render the lookahead point.
+
+        Parameters
+        ----------
+        e : EnvRenderer
+            environment renderer
         """
         if self.lookahead_point is not None:
             points = self.lookahead_point[:2][None]  # shape (1, 2)
@@ -76,6 +109,11 @@ class PurePursuitPlanner:
     def render_local_plan(self, e):
         """
         update waypoints being drawn by EnvRenderer
+
+        Parameters
+        ----------
+        e : EnvRenderer
+            environment renderer
         """
         if self.current_index is not None:
             points = self.waypoints[self.current_index : self.current_index + 10, :2]
@@ -85,12 +123,14 @@ class PurePursuitPlanner:
         """
         Finds the current waypoint on the look ahead circle intersection
 
-        Args:
+        Parameters
+        ----------
             lookahead_distance (float): lookahead distance to find next point to track
             position (numpy.ndarray (2, )): current position of the vehicle (x, y)
             theta (float): current vehicle heading
 
-        Returns:
+        Returns
+        -------
             current_waypoint (numpy.ndarray (3, )): selected waypoint (x, y, velocity), None if no point is found
         """
 
@@ -118,34 +158,34 @@ class PurePursuitPlanner:
         else:
             return None
 
-    def plan(self, pose_x, pose_y, pose_theta, lookahead_distance, waypoints=None):
+    def plan(self, state: dict) -> np.ndarray:
         """
-        Planner plan function overload for Pure Pursuit, returns acutation based on current state
+        Calculate the desired steering angle and speed based on the current vehicle state
 
-        Args:
-            pose_x (float): current vehicle x position
-            pose_y (float): current vehicle y position
-            pose_theta (float): current vehicle heading angle
-            lookahead_distance (float): lookahead distance to find next waypoint to track
-            waypoints (numpy.ndarray [N x 4], optional): list of dynamic waypoints to track, columns are [x, y, velocity, heading]
+        Parameters
+        ----------
+        state : dict
+            observation as returned from the environment.
 
-        Returns:
+        Returns
+        -------
             speed (float): commanded vehicle longitudinal velocity
             steering_angle (float):  commanded vehicle steering angle
+
+        Raises
+        ------
+        ValueError
+            if waypoints are not set
         """
-        if waypoints is not None:
-            if waypoints.shape[1] < 3 or len(waypoints.shape) != 2:
-                raise ValueError("Waypoints needs to be a (Nxm), m >= 3, numpy array!")
-            self.waypoints = waypoints
-        else:
-            if self.waypoints is None:
-                raise ValueError(
-                    "Please set waypoints to track during planner instantiation or when calling plan()"
-                )
-        position = np.array([pose_x, pose_y])
-        lookahead_distance = np.float32(lookahead_distance)
+        if self.waypoints is None:
+            raise ValueError(
+                "Please set waypoints to track during planner instantiation or when calling plan()"
+            )
+        position = np.array([state["pose_x"], state["pose_y"]])
+        pose_theta = state["pose_theta"]
+
         self.lookahead_point = self._get_current_waypoint(
-            lookahead_distance, position, pose_theta
+            self.lookahead_distance, position, pose_theta
         )
 
         if self.lookahead_point is None:
@@ -156,7 +196,7 @@ class PurePursuitPlanner:
             pose_theta,
             self.lookahead_point,
             position,
-            lookahead_distance,
+            self.lookahead_distance,
             self.wheelbase,
         )
 
