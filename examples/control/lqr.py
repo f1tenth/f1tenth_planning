e718693c6c8c4dc168c580cf5b4d9c2c9bc4f88a
Ahmad Amine <AhmadAmine998@gmail.com>
2024-04-26 03:47:02 -0400

    Update LQR example to v1.0.0 with new LQR implementation

--- examples/control/lqr.py
+++ examples/control/lqr.py
@@ -1,36 +1,6 @@
-# MIT License
-
-# Copyright (c) Hongrui Zheng, Johannes Betz
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-"""
-LQR waypoint tracker example
-
-Author: Hongrui Zheng
-Last Modified: 5/5/22
-"""
-
 import numpy as np
 import gymnasium as gym
-import f110_gym
-from f1tenth_planning.control.lqr.lqr import LQRPlanner
+from f1tenth_planning.control.lqr.lqr import LQRController
 
 
 def main():
@@ -41,7 +11,7 @@ def main():
 
     # create environment
     env = gym.make(
-        "f110_gym:f110-v0",
+        "f1tenth_gym:f1tenth-v0",
         config={
             "map": "Spielberg",
             "num_agents": 1,
@@ -51,16 +21,12 @@ def main():
         render_mode="human",
     )
 
-    # create planner
-    raceline = env.unwrapped.track.raceline
-    waypoints = np.stack(
-        [raceline.xs, raceline.ys, raceline.vxs, raceline.yaws, raceline.ks], axis=1
-    )
-    planner = LQRPlanner(waypoints=waypoints)
+    # create controller
+    controller = LQRController(env.unwrapped.track)
 
-    env.add_render_callback(planner.render_waypoints)
-    env.add_render_callback(planner.render_local_plan)
-    env.add_render_callback(planner.render_closest_point)
+    env.add_render_callback(controller.render_waypoints)
+    env.add_render_callback(controller.render_local_plan)
+    env.add_render_callback(controller.render_closest_point)
 
     # reset environment
     poses = np.array(
@@ -79,12 +45,7 @@ def main():
     # run simulation
     laptime = 0.0
     while not done:
-        steer, speed = planner.plan(
-            obs["agent_0"]["pose_x"],
-            obs["agent_0"]["pose_y"],
-            obs["agent_0"]["pose_theta"],
-            obs["agent_0"]["linear_vel_x"],
-        )
+        steer, speed = controller.plan(obs['agent_0'])
         obs, timestep, terminated, truncated, infos = env.step(
             np.array([[steer, speed]])
         )
